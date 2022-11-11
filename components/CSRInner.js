13d29f81adc5e295b537215b79069ed4a2ffbed6
yamato <ymtakt.1229@gmail.com>
2022-11-11 22:51:35 +0900

    修正

---
+++ components/CSRInner.js
@@ -0,0 +1,5 @@
+const CSRInner = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default CSRInner;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import { RecoilRoot } from "recoil"
 
+
+import CSR from '../components/CSR';
+// import type { AppProps } from 'next/app';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <CSR>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </CSR>
   );
 }
 
